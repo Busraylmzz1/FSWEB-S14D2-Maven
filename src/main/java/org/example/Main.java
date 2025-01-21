184c5a747d3ded64f367f9a979b4b924e6ed7318
Büşra Yılmaz <124158606+Busraylmzz1@users.noreply.github.com>
2025-01-21 14:34:49 +0300

    new

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import org.example.model.*;
 
 public class Main {
     public static void main(String[] args) {
-        Lamp lamp= new Lamp(100, true, LampType.NEON);
+        Lamp lamp= new Lamp(100, true, LampType.NORMAL);
         Celling celling= new Celling(30, PaintColor.WHİTE);
         Bed bed = new Bed("double", 2,2,2,2);
         Wall northWall= new Wall("north");
@@ -12,7 +12,7 @@ public class Main {
         Wall westWall = new Wall("West");
         Wall southWall = new Wall("South");
         Wall[] walls = new Wall[]{northWall,eastWall,southWall,westWall};
-        Bedroom bedroom = new Bedroom( "bedroom1", northWall, eastWall, westWall, southWall,celling, bed,lamp, new Wardrobe(2, 3, 4), new Carpet(2,5,PaintColor.WHİTE));
+        Bedroom bedroom = new Bedroom( "bedroom1", northWall, eastWall, westWall, southWall,celling, bed,lamp, new Wardrobe(2, 3, 4), new Carpet(2,5,PaintColor.RED));
 
        // bedroom.createBedroom; burdayımmmm
     }
